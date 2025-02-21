7b8d0e30f7e307d2ee6f90cb79241a5196e0e3cb
tejashwinn. <tejashwin5104@gmail.com>
2025-02-21 18:03:51 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# task-queue
+In-process task queue for java applications
---
+++ src/main/java/com/tejashwin/task/queue/TaskQueue.java
@@ -0,0 +1,8 @@
+package com.tejashwin.task.queue;
+
+public class TaskQueue {
+
+  public static void main(String[] args) {
+    System.out.println("init");
+  }
+}
